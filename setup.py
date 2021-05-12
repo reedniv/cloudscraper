4abc9dabdedf357338bad142350e2e98eb80ad8d
reedniv <26852596+reedniv@users.noreply.github.com>
2021-05-12 07:16:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'requests >= 2.9.2',
         'requests_toolbelt >= 0.9.1',
         'pyparsing >= 2.4.7'
+        'aiohttp == 3.7.3'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
